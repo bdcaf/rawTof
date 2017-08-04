8283567c55333fb236e7a516c64fd0698674c7f8
Clemens Ager <clemens.ager@gmail.com>
2017-08-04 09:47:40 +0200

    sum spec

--- R/plain.R
+++ R/plain.R
@@ -1,8 +1,3 @@
-library(rhdf5)
-
-tof.h5 <- "data/PTRTOF_test_spectrum.h5"
-fid <- H5Fopen(tof.h5)
-
 get_peak_matrix <- function(fid){
   peak.data <- H5Dread(H5Dopen(fid, "PeakData/PeakTable"))
   peak.value <- H5Dread(H5Dopen(fid, "PeakData/PeakData"))
@@ -11,6 +6,26 @@ get_peak_matrix <- function(fid){
   peak.matrix
 }
 
+get_sum_spec <- function(fid) H5Dread(H5Dopen(fid, "FullSpectra/SumSpectrum"))
+
+masscal_helper <- function(fid){
+  gr <- h5readAttributes(fid, "FullSpectra")
+  with(gr,
+       list( to_mass = Vectorize(function(i) ( (i-`MassCalibration p2`)/`MassCalibration p1`)^2),
+             to_index = Vectorize(function(m) `MassCalibration p2` + `MassCalibration p1`*sqrt(m))
+             ))
+}
+
+library(rhdf5)
+
+tof.h5 <- "data/PTRTOF_test_spectrum.h5"
+fid <- H5Fopen(tof.h5)
+
 peaktable <- get_peak_matrix(fid)
-plot(peaktable[69, ])
-plot(peaktable[45, ])
+plot(peaktable[69, ], type="l")
+plot(peaktable[45, ], type="l")
+
+mh <- masscal_helper(fid)
+ss <- get_sum_spec(fid)
+mass_scale <- mh$to_mass(seq_along(ss))
+plot(mass_scale, ss, type="l")
