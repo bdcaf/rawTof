4b1465f5eca97b56d286f727e7e91e39ea1bcd47
Clemens Ager <clemens.ager@gmail.com>
2017-08-05 14:02:35 +0200

    code

--- R/plain.R
+++ R/plain.R
@@ -1,3 +1,6 @@
+#' retrieve matrix of precalculated peaks
+#' @export
+#' @param fid h5d file handle
 get_peak_matrix <- function(fid){
   peak.data <- H5Dread(H5Dopen(fid, "PeakData/PeakTable"))
   peak.value <- H5Dread(H5Dopen(fid, "PeakData/PeakData"))
@@ -6,8 +9,14 @@ get_peak_matrix <- function(fid){
   peak.matrix
 }
 
+#' retrieve sum spectrum
+#' @export
+#' @param fid h5d file handle
 get_sum_spec <- function(fid) H5Dread(H5Dopen(fid, "FullSpectra/SumSpectrum"))
 
+#' create functions to work with existing mass calibration
+#' @export
+#' @param fid h5d file handle
 masscal_helper <- function(fid){
   gr <- h5readAttributes(fid, "FullSpectra")
   with(gr,
@@ -16,13 +25,42 @@ masscal_helper <- function(fid){
              ))
 }
 
-dimhelper <- function(i, wid) c(i %% wid +1, i %/% wid)
+dimhelper <- function(i, wid) c(i %% wid, i %/% wid)
+dim_calc <- function(wanted, diml){
+  # zero indexed calculation
+  res <- rep(NA, length(diml))
+  cu <- c(0, wanted-1)
+  for (i in seq_along(diml)){
+    cu <- dimhelper(cu[[2]], diml[[i]])
+    res[i] <- cu[[1]]
+  }
+  res + 1
+}
 
+#' extract a single scan from tof spectrum
+#'
+#' @description Note: this function does not check whether the scan is in the range
+#' of measurement.  Checks will need to be done manually!
+#' Note2: Theoretically the full matrix of scans could be loaded similar
+#' to get_peak_matrix - but this will take loads of memory and usually
+#' is not desirable.
+#' @export
+#' @param fid h5d file handle
+#' @param id index of scan
+#' @return vector of single scan
 get_single_scan <- function(fid, id){
-  tofblock <- H5Dopen(H5Gopen(fid, "FullSpectra"), 
-                      "TofData")
-  h5spaceFile <- H5Dget_space(tofblock)
-  dims <- H5Sget_simple_extent_dims(h5spaceFile)
+  tofblock <- H5Dopen(H5Gopen(fid, "FullSpectra"), "TofData")
+  h5space <- H5Dget_space(tofblock)
+  dims <- H5Sget_simple_extent_dims(h5space)
+  h5spacemem <- H5Screate_simple(dims$size[[1]])
+
+  pos <- dim_calc(id, dims$size[-1])
+  slab <- H5Sselect_hyperslab(h5space,
+                              start = c(1, pos),
+                              count = c(dims$size[[1]], rep(1, 3)) )
+  H5Dread(h5dataset = tofblock,
+          h5spaceFile = h5space,
+          h5spaceMem = h5spacemem )
 }
 
 library(rhdf5)
@@ -35,13 +73,15 @@ plot(peaktable[69, ], type="l")
 plot(peaktable[45, ], type="l")
 
 mh <- masscal_helper(fid)
-ss <- get_sum_spec(fid)
+ss <- get_sum_spec(fid)/180
+s2 <- get_single_scan(fid, 31)
 mass_scale <- mh$to_mass(seq_along(ss))
-plot(mass_scale, ss, type="l")
+plot(mass_scale, s2, type="l")
+lines(mass_scale, ss, col="red")
 
 mass_range <- mh$to_index(c(40, 50))
 selected <- seq(from = floor(mass_range[1]),
                 to = ceiling(mass_range[2]))
 
-plot(mass_scale[selected], ss[selected], type="l")
-
+plot(mass_scale[selected], s2[selected], type="l")
+lines(mass_scale[selected], ss[selected], col="red")
