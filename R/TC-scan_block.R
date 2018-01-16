5f2eca91b34817c28816b7339628618e03e44989
Clemens Ager <clemens.ager@gmail.com>
2018-01-16 17:43:03 +0100

    small adjustment

--- R/TC-scan_block.R
+++ R/TC-scan_block.R
@@ -15,12 +15,11 @@ setGeneric(name = "scan_block", def = function(.Object, indrange) {
 setMethod("scan_block", "TofClass",
           function(.Object, indrange){
             dd <- tof_ob@.dims
+            block_size = c(indrange[[2]], dd[[2]], dd[[3]], dd[[4]])
             H5Sselect_hyperslab(.Object@.scanspace,
                                 start = c(indrange[[1]], 1, 1, 1),
-                                count = c(indrange[[2]],
-                                          dd[[2]], dd[[3]], dd[[4]]) )
-            h5spacemem <- H5Screate_simple(
-                       c(indrange[[2]], dd[[2]], dd[[3]], dd[[4]]))
+                                count = c(block_size) )
+            h5spacemem <- H5Screate_simple(block_size)
             block <- H5Dread(h5dataset = .Object@.scandata,
                              h5spaceFile = .Object@.scanspace,
                              h5spaceMem = h5spacemem) %>%
