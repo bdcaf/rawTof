eaaf7e2d176cb0f714b014475a8d607952753dd4
Clemens Ager <clemens.ager@gmail.com>
2018-11-14 16:15:03 +0100

    filename to fid

--- R/peak_matrix.R
+++ R/peak_matrix.R
@@ -6,12 +6,12 @@
 #' @return matrix of all peaks captured in Ionicion sofrtware
 #' @examples
 #' \dontrun{
-#' get_peak_matrix("tof_file.h5")
+#' get_peak_matrix(fid)
 #' }
 get_peak_matrix <- function(fid){
   peak_table <- h5read(fid, "PeakData/PeakTable",
                        read.attributes = T)
-  peak_data <- h5read(tof_file, "PeakData/PeakData")
+  peak_data <- h5read(fid, "PeakData/PeakData")
   peak_matrix <- matrix(peak_data,
                         nrow = dim(peak_data)[1])
   rownames(peak_matrix) <- with(peak_table, paste(label, mass))
